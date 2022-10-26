2e2c81409860a04405ee018829325b169622b080
jhyungkim <dnlwmxj135@gmail.com>
2022-10-26 10:59:41 +0900

    update code

--- src/main/java/com/campus/controller/AdminMemberInfoController.java
+++ src/main/java/com/campus/controller/AdminMemberInfoController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MemberInfoController {
+public class AdminMemberInfoController {
 	
 	@GetMapping(path= {"/member-info.action"})
 	public String memberInfo() {
