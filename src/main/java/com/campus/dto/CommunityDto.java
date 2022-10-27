db0a54f2393bbe7ccb8304898412f695a3b3a371
ys <ys@ys>
2022-10-27 11:41:01 +0900

    update

--- src/main/java/com/campus/dto/CommunityDto.java
+++ src/main/java/com/campus/dto/CommunityDto.java
@@ -1,5 +1,8 @@
 package com.campus.dto;
 
+import lombok.Data;
+
+@Data
 public class CommunityDto {
 	
 	private int boardNo;
--- src/main/java/com/campus/mapper/CommunityMapper.java
+++ src/main/java/com/campus/mapper/CommunityMapper.java
@@ -1,8 +1,16 @@
 package com.campus.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import com.campus.dto.BoardDto;
 
 @Mapper
 public interface CommunityMapper {
+	
+	@Insert("INSERT INTO board (title, memberId, content, category) VALUES (#{title},#{memberId}, #{content}, #{category})")
+	//@Options(useGeneratedKeys = true, keyColumn = "boardno", keyProperty = "boardNo")
+	void insertBoard(BoardDto board);
 
 }
